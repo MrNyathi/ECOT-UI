85a36a3d70279412e56caf8468589aded1701656
Mavuso Sandziso <chris24ncele@gmail.com>
2024-12-04 20:15:03 +0200

    I've setup the footer in one place too

---
+++ templates/footer.php
@@ -0,0 +1,34 @@
+<!-- Footer Section -->
+<footer class="footer">
+      <div>
+        <h4>Address</h4>
+        <p>PO BOX 178, <br />MBABANE</p>
+        <p>SANDA, KA SIBENTA</p>
+      </div>
+      <div>
+        <h4>Contact Us</h4>
+        <p>Telephone: <br />+25478667744</p>
+        <p>Email: ecot@info.co.sz</p>
+      </div>
+      <div>
+        <form>
+          <h4>Subscribe to our website</h4>
+          <input type="email" placeholder="Enter your email" required />
+          <button type="submit">Subscribe</button>
+        </form>
+      </div>
+    </footer>
+    <div class="end">
+      <div class="left">
+        &copy; 2024 ECOT
+        <ul>
+          <li>Privacy</li>
+          <li>Terms</li>
+        </ul>
+      </div>
+      <div class="right">
+        ESWATINI COLLEGE OF TECHNOLOGY
+        <img src="img/web.png" alt="" />
+        <img src="img/fb.png" alt="" />
+      </div>
+    </div>
